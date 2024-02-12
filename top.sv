b3141b09ba73635ff9c32fac073eb45315b4274c
ZohebMir1 <110513499+ZohebMir1@users.noreply.github.com>
2024-02-11 23:53:20 -0800

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,27 +1,40 @@
 interface mcp(input clk);
-  logic [17:0] instr;
-  logic rst;
-  logic start;
+  logic [27:0] instr1;
+  logic [27:0] instr2;
+  logic [27:0] instr3;
   logic [7:0] mem_out;
   logic [7:0] reg_out;
+  logic rst;
+  logic start1; 
+  logic start2; 
+  logic start3;
   logic done;
   
+  
   clocking cb@(posedge clk);
-    output instr;
-    output rst;
-    output start;
-    input mem_out;
-    input reg_out;
-    input done;
+  output instr1;
+  output instr2;
+  output instr3;
+  input mem_out;
+  input reg_out;
+  output rst;
+  output start1; 
+  output start2; 
+  output start3;
+  input done;
   endclocking
   
   clocking mcb@(posedge clk);
-    input instr;
-    input rst;
-    input start;
-    input mem_out;
-    input reg_out;
-    input done;
+  input instr1;
+  input instr2;
+  input instr3;
+  input mem_out;
+  input reg_out;
+  input rst;
+  input start1; 
+  input start2; 
+  input start3;
+  input done;
   endclocking
   
   modport tb_modport (clocking cb);
@@ -38,11 +51,15 @@ module top;
   
   mcp mcp_inst(clk);
   
-  multi_cp_design dut_inst(.clk(clk), .mem_out(mcp_inst.mem_out), .reg_out(mcp_inst.reg_out), 
+  core dut_inst(.clk(clk),
 .done(mcp_inst.done), 
-.rst(mcp_inst.rst), 
-.start(mcp_inst.start), 
-.instr(mcp_inst.instr));
+.rst_n(mcp_inst.rst), 
+                           .start1(mcp_inst.start1),
+                           .start2(mcp_inst.start2),
+                           .start3(mcp_inst.start3),
+                           .inst1(mcp_inst.instr1),
+                           .inst2(mcp_inst.instr2),
+                           .inst3(mcp_inst.instr3));
 
 testbench  tb_inst(.vif(mcp_inst));
   
