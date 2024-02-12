de18b63fb01de7d36538e3f57e5862270d8d90bc
ZohebMir1 <110513499+ZohebMir1@users.noreply.github.com>
2024-02-11 23:54:45 -0800

    Update imon.sv

--- imon.sv
+++ imon.sv
@@ -1,12 +1,12 @@
 class imon;
   packet pkt;
   out_packet opkt;
-  virtual mcp_inst.tb_mon vif;
-  mailbox#(packet) mbx;
+  virtual mcp.tb_mon vif;
+  mailbox#(out_packet) mbx;
   
   bit [31:0] no_of_pkts_rcvd;
   
-  function new(input mailbox#(packet) mbx_arg, input virtual mcp_inst.tb_mon vif_arg);
+  function new(input mailbox#(packet) mbx_arg, input virtual mcp.tb_mon vif_arg);
     this.mbx=mbx_arg;
     this.vif = vif_arg;
   endfunction
@@ -14,10 +14,10 @@ class imon;
   task run();
     $display("[iMon] run started at time=%0t ",$time); 
     forever begin
-      @(posedge vif.mcb.start);
+      @(posedge vif.mcb.start1);
       no_of_pkts_rcvd++;
-      $display("[iMon] Started collecting packet %0d at time=%0t ",no_of_pkts_recvd,$time); 
-      opkt.cal_out(vif.mcb.instr);
+      $display("[iMon] Started collecting packet %0d at time=%0t ",no_of_pkts_rcvd,$time); 
+      opkt.cal_out(vif.mcb.instr1);
       mbx.put(opkt);
       end
     $display("[iMon] run ended at time=%0t ",$time);
