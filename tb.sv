f51fecb5d3f4e72bfe36dd0a952819f1f21ce4f5
ZohebMir1 <110513499+ZohebMir1@users.noreply.github.com>
2024-02-11 23:49:18 -0800

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -1,5 +1,5 @@
 
-program testbench(mcp_inst vif);
+program testbench(mcp vif);
 
 //Section 4.1 : Include test cases
 `include "test.sv"
@@ -7,17 +7,40 @@ program testbench(mcp_inst vif);
 
 //Section 4.2 : Define test class handles
 base_test test;
-
-
+packet pkt;
+  out_packet opkt;
+int t =0;
 //Section 6: Verification Flow
 initial begin
+  pkt=new();
 $display("[Program Block] Simulation Started at time=%0t",$time);
 //Section 6.1 : Construct test object and pass required interface handles
- test = new(vif.tb_mod_port,vif.tb_mon,vif.tb_mon);
-
+ test = new(vif.tb_modport,vif.tb_mon,vif.tb_mon);
+  
+  $display("[Driver] Driving Reset transaction into DUT at time=%0t",$time);
+  vif.rst <= 1'b1;
+  #10;
+  vif.rst <= 1'b0;
+  $display("[Driver] Driving Reset transaction completed at time=%0t",$time); 
+  
+  repeat(30) begin
+    t++;
+       pkt.randomize();
+    $display("[Generator] Sending packet %0d with value for instr1= %h,instr2= %h,instr3= %h, to driver",t,pkt.instr_pkt1,pkt.instr_pkt2,pkt.instr_pkt3);
+    $display("[Driver] Received packet %0d from generator at time=%0t",t,$time);
+    $display("[Driver] Driving packet %0d",t);
+ 
+    vif.cb.start1<=1;
+    vif.cb.start2<=1;
+    vif.cb.start3<=1;
+   	vif.cb.instr1<=pkt.instr_pkt1;
+    vif.cb.instr2<=pkt.instr_pkt2;
+    vif.cb.instr3<=pkt.instr_pkt3;
+    $display("[Driver] Driving packet %0d",t);
+  end
 //Section 6.2 : Start the testcase.
 //test.run();
-$display("[Program Block] Simulation Finished at time=%0t",$time);
+$display("[Program Block] Simulation Finished");
 end
 
 endprogram
